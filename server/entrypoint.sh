6571f38345fb83d0e9ea71a52a47339215c50344
Pal1n0 <pavoldzama@gmail.com>
2025-10-03 12:22:28 -0400

    final touch

---
+++ server/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec "$@"
