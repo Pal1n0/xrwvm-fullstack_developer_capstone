f575b19e7764c3b593f60dc32a55c4d70b59e230
Pal1n0 <pavoldzama@gmail.com>
2025-10-03 08:22:53 -0400

    linting 1

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -9,15 +9,18 @@ class CarModelInline(admin.TabularInline):  # or admin.StackedInline
     model = CarModel
     extra = 1  # how many empty forms to show
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'car_make', )  # columns to show
     list_filter = ('car_make',)          # add filter by car_make
     search_fields = ('car_make__name',)  # allow searching by make name
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
